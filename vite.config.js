30341861661a58812eedf3db618aea73c096af21
waritinan <warittinan.khun@kmutt.ac.th>
2023-02-08 13:22:10 +0700

    ssssss

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  base:'/testdepoly/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url ))
+  //   }
+  // }
 })
 
